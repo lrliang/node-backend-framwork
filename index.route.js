95e9d7e333fc52d8284903e7649e96455e572745
Liang <392425595@qq.com>
2019-03-12 23:38:29 +0800

    [LLR][unused commit for webhook test ]

--- index.route.js
+++ index.route.js
@@ -6,6 +6,7 @@ const router = express.Router(); // eslint-disable-line new-cap
 /** GET /health-check - Check service health */
 router.get('/health-check', (req, res) => res.send('health'));
 
+
 // use glob to match *.route files
 router.get('/*', (req, res) => res.send('Unkown Path!'));
 
